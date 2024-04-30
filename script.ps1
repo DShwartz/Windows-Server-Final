e77a7a0030e1485fe6120a52dd2ee95940588ebb
DShwartz <dshwartz@student.qcc.edu>
2024-04-30 09:20:46 -0400

    bruh

---
+++ script.ps1
@@ -0,0 +1,8 @@
+#Requires -RunAsAdministrator
+
+# Let script be able to access AD stuff
+Import-Module ActiveDirectory
+
+# TODO: make variable that auto detects domain and sets a variable for it
+#       add comments for stuff
+#       make the actual script
